32d478f08732c455d40a520372b8c3fe5eebe3a7
je-boska <joneirikboska@gmail.com>
2022-08-28 18:35:17 +0200

    Convert to .mdx, use astro component in post

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,13 +1,7 @@
 import React, { useState } from 'react';
 
-export default function Dropdown({
-  label,
-  children,
-}: {
-  label: string;
-  children: React.ReactNode;
-}) {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+export default function Dropdown({ label, children }) {
+  const [isOpen, setIsOpen] = useState(false);
 
   if (isOpen) return <>{children}</>;
   else return <button onClick={() => setIsOpen(true)}>{label}</button>;
